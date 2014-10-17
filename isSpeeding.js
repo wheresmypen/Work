0d96b446525298b4fa27ebc75bb7d8a2b6d59717
Daniel O'Brien <wheresmypen@yahoo.com>
2014-10-16 18:27:17 -0600

    first exercise done

---
+++ isSpeeding.js
@@ -0,0 +1,28 @@
+
+
+
+function isSpeeding(speed, isBirthday){
+
+if (isBirthday){
+    speed =  speed -5;
+}
+
+var penalty = 0;
+
+
+    if (speed > 60){
+        penalty++;
+    }
+
+    if (speed > 80){
+        penalty++;
+    }
+
+
+
+return penalty;
+}
+
+
+var copSays = isSpeeding(86, true);
+console.log(copSays);
