e53458bdd076e03c5971ed63f6fec325f761cbd9
Daniel O'Brien <wheresmypen@yahoo.com>
2014-10-21 21:08:05 -0600

    Folder tabs working with linked panels

---
+++ OCT21/folder.js
@@ -0,0 +1,64 @@
+
+$(function(){
+
+    var wrapper = $('.tab-wrapper');
+    attachTabs(wrapper);
+
+
+/*
+    wrappers.each(function(){
+
+        var wrapper = $(this);
+        attachTabs(wrapper);
+
+    })
+*/
+
+
+
+
+
+    function attachTabs(wrapper){
+        var tabs = wrapper.find('ul.tabs li');
+        var panels = wrapper.find('.panels .panel');
+
+        console.log(tabs);
+        console.log(panels);
+
+
+        tabs.on('click', function(event){
+
+            tabs.removeClass('active');
+            panels.removeClass('active');
+
+            var current_tab = this;
+
+            $(current_tab).addClass('active');
+
+            tabs.each(function(idx){
+              if(current_tab == this){
+                  $(panels.get(idx)).addClass('active');
+              }
+            })
+
+
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
