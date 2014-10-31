b5bf0dbbe3e03e7c5c156b5c214a3773082019d3
Daniel O'Brien <wheresmypen@yahoo.com>
2014-10-30 18:42:04 -0600

    Added localStorage memory to this exercise

--- OCT29/userTable.js
+++ OCT29/userTable.js
@@ -10,13 +10,38 @@ $(function(){
     var clearAll = $('#clearAll');
 
 
+    // Check to see if we have existing records
+    var userTableData = localStorage.getItem('userTable');
+    userTableData = JSON.parse(userTableData);
+
+    if(!userTableData){
+        userTableData =[];
+    } else {
+        userTableData.forEach(function(record){
+            tbody.append( makeRow(record) );
+        });
+    }
+
+
+
     addButton.on('click', function(){
 
         var name = nameField.val();
         var email = emailField.val();
 
         if(email.match(/^.+?\@.+\..+?$/)){ // is valid email ?
-            tbody.append( getRow(name, email) );
+
+            var record = {
+                name: name
+              , email: email
+              , ts: Date.now()
+            };
+
+            userTableData.push(record);
+
+            save();
+
+            tbody.append( makeRow(record) );
 
             nameField.val("");
             emailField.val("").css({borderColor: ''});
@@ -29,17 +54,33 @@ $(function(){
     });
 
     tbody.on('click', 'button.remove', function(event){
-        $(this).parents('tr').remove();
+        var row = $(this).parents('tr');
+        var id = row.data('id');
+
+        userTableData = userTableData.filter(function(record){
+            return (record.ts !== id);
+        });
+
+        save();
+
+        row.remove();
     });
 
     clearAll.on('click', function(){
         tbody.empty();
+        userTableData = [];
+        save();
     });
 
-    function getRow(name, email){
-        return "<tr>" +
-            "<td>" + name + "</td>" +
-            "<td>" + email + "</td>" +
+
+    function save(){
+        localStorage.setItem('userTable', JSON.stringify(userTableData));
+    }
+
+    function makeRow(record){
+        return "<tr data-id='" + record.ts +"'>" +
+            "<td>" + record.name + "</td>" +
+            "<td>" + record.email + "</td>" +
             "<td><button class='remove btn btn-primary btn-xs'>remove</button></td>" +
             "</tr>";
     }
