43a79bec49cc4a1adfe07bbcbb9f47e97561c2d5
Daniel O'Brien <wheresmypen@yahoo.com>
2014-11-13 00:36:31 -0700

    Worked out details regarding homework 11-12

--- Nov11/js/script.js
+++ Nov11/js/script.js
@@ -1,22 +1,25 @@
-// WORKS AS FAR AS ADD BUTTON
+// WORKS AS FAR AS DESCRIPTION WITHOUT EXTRAS
 
 $(function() {
     var employees = [
         {
 
             name: "David",
-            phone: "800-555-5555"
+            phone: "800-555-5555",
+            address: "123 Pleasant St, Morgantown, WV 26505"
 
 
         },
 
         {
             name: "Bob",
-            phone: "303-123-4567"
+            phone: "303-123-4567",
+            address: "1090 Ithica Dr, Boulder, CO 80305"
+
         }
 
 
-    ]
+    ];
 
 
 //    MAIN
@@ -24,13 +27,105 @@ $(function() {
     myApp();
 
     function myApp() {
-        $('#employee_list').html(render_employee_table(employees))
+        $('#employee_list').html(render_employee_table(employees));
 
         $('#add').click(function(){
-            render_edit_box();
+
+            render_edit_box('add');
+//            $('#edit_box').removeClass('closed');
+            $('#edit_box').slideDown();
+
         });
+    };
+
+    function terminate_employee(index){
+
+        console.log(index);
+        employees.splice(index, 1);
+
+        render_employee_table(employees);
+
+    };
+
+
+    //    Render Employee
+
+    function render_edit_box(type, employee, index){
+
+        var prename="";
+        var prephone="";
+        var preaddress="";
+
+        if (type == "edit"){
+
+            prename = employee.name;
+            prephone = employee.phone;
+            preaddress = employee.address;
+
+        };
+
+        var html = '<div><label>name</label><input id="edit_name" value="'
+            + prename + '"></div><div><lable>phone</lable><input id="edit_phone" value = "'
+            + prephone + '"></div><div><lable>address</lable><input id ="edit_address" value = "'
+            + preaddress + '"</div>';
+
+        var button_name = type ==  "add" ? "add it" : "update it";
+
+        html += "<button id='saveit'>" + button_name + "</button>";
+
+        $('#edit_box').html(html);
+
+        $("#saveit").click(function(){
+
+
+            var e = {
+                name : $("#edit_name").val(),
+                phone : $("#edit_phone").val(),
+                address : $("#edit_address").val()
+            };
+
+            if (type == "edit"){
+
+                update_employee(e, index);
+
+            }
+
+
+            else if (type == "add"){
+
+                add_employee(e);
+            }
+
+
+            $('#edit_box').slideUp();
+
+
+        })
+
+
+
+
     }
 
+    function update_employee(employee, index){
+
+        employees[index] = employee;
+        render_employee_table(employees);
+
+    }
+
+
+    function add_employee(employee){
+        employees.push(employee);
+        var p = render_employee_table(employees);
+
+//
+//            THIS ARE IS LIKELY SOME DATA THAT NEEDS TO BE MOVED
+//            TO RENDER TABLE OR SOMEWHERE ELSE
+//
+
+        $('#employee_list').html(p);
+    }
 
 //    Render Table
 
@@ -39,48 +134,38 @@ $(function() {
         var html;
         html = "<table>"
 
-        data.forEach(function(e){
+        data.forEach(function(e, index){
 
             html += "<tr>";
             html += "<td>" + e.name + "</td>";
             html += "<td>" + e.phone + "</td>";
+            html += "<td>" + e.address + "</td>";
+            html += "<td><button class = 'delete' index ='"+index+"'>Del</button></td>";
+            html += "<td><button class = 'edit' index ='"+index+"'>Edit</button></td>";
             html += "</tr>";
 
         })
 
-        html += "</table>"
+        html += "</table>";
 
-        return html;
+        $('#employee_list').html(html);
 
-    }
+        $('.delete').click(function(){
+            console.log('delete clicked');
+            terminate_employee($(this).attr("index"));
+        });
 
+        $('.edit').click(function(){
+            console.log('edit clicked');
 
-//    Render Employee
+            render_edit_box("edit", employees[$(this).attr("index")], $(this).attr("index"));
 
-    function render_edit_box(){
-
-       var html = '<div></div><label>name</label><input id="edit_name"></div><div><lable>phone</lable><input id="edit_phone"></div>';
-
-       html += "<button id='addit'>Add It</button>";
-
-        $('#edit_box').html(html);
-
-       $("#addit").click(function(){
-
-           var e = {
-               name : $("#edit_name").val(),
-               phone : $("#edit_phone").val()
-           }
-
-           employees.push(e);
-           var p = render_employee_table(employees);
-           $('#employee_list').html(p);
-           $('#edit_box').html('');
-       })
-
-
+        })
 
     }
+
+
+
 
 
 });
