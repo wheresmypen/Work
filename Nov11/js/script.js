fa6d9ed84f16069293ccf28302542e829b6db81e
Daniel O'Brien <wheresmypen@yahoo.com>
2014-11-18 04:41:18 -0700

    FUNCTIONAL C.R.U.D. employees on 11-18

--- Nov11/js/script.js
+++ Nov11/js/script.js
@@ -1,23 +1,5 @@
 $(function() {
-    var employees = [
-        {
-
-            name: "David",
-            phone: "800-555-5555",
-            address: "123 Pleasant St, Morgantown, WV 26505"
-
-
-        },
-
-        {
-            name: "Bob",
-            phone: "303-123-4567",
-            address: "1090 Ithica Dr, Boulder, CO 80305"
-
-        }
-
-
-    ];
+    var employees = [];
 
 //  Flag for closing the map
 
@@ -53,22 +35,13 @@ $(function() {
 
     };
 
-//  Function that removes data from table - functionality broken because rendering of static object removed
 
-    function terminate_employee(index){
-
-        console.log(index);
-        employees.splice(index, 1);
-
-        render_employee_table(employees);
-
-    };
-
-
-    //    Render Employee
+//      Render Employee
 //    both adds and edits employee
 
-    function render_edit_box(type, employee, index){
+    function render_edit_box(type, employee, index, id){
+
+        console.log(type, employee, index, id);
 
 //        creates empty field for form
 
@@ -115,14 +88,13 @@ $(function() {
 
 //              runs functionality to replace edited object
 
-                update_employee(e, index);
+                update_employee(e, index, id);
 
             }
 
 //                runs functionality to add new object to database
 
             else if (type == "add"){
-                console.log(e);
                 add_employee(e);
             }
 
@@ -137,68 +109,113 @@ $(function() {
 
     }
 
-    function update_employee(employee, index){
+    function update_employee(employee, index, id){
+
+        console.log (employee, index, id);
+
+        updateURL = 'http://69.164.197.6/employees/'+id;
 
 //
-//        Function that replaces data in table - functionality broken because rendering of static object removed
-//
+//        Function that replaces data in table
 //        Needs to be created with PUT request
 
-        employees[index] = employee;
-        render_employee_table(employees);
+        $.ajax({
+            type: "POST",
+            url: updateURL,
+            data: employee
+        })
+//        WHEN SUCCESSFUL CALL IS MADE, PROMISES
+//            ARE INVOKED TO RE-RENDER TABLE
+            .done(function() {
+//              MAKES AN AJAX REQUEST TO GET COMPLETE TABLE DATA FROM DATABASE
+                $.ajax({
+                    url: 'http://69.164.197.6/employees/',
+                    success: function(employees){
+//                CALLS FUNCTION TO RE-RENDER DOM WITH COMPLETE TABLE
+                        render_employee_table(employees);
+                    }
+                })
+            });
+
+        console.log(employee);
+//        render_employee_table(employees);
 
     }
 
+//          This is the beginning of a large amount of code that renders
+//          a google map centered on the address of the point selected
+//          the employee object that was clicked and the index
+//          number are passed to the function
+
     function render_map(employee, index){
 
-        console.log(employee.address);
+//          This enables the area for map rendering to appear in the DOM
 
-        $("#map-canvas").show();
+        $("#map-canvas").fadeIn();
 
         var address = employee.address;
+
+//          Calls the function that derives a LAT/LNG coordinate from an address
 
         getGeo(address, locatePoint);
 
         function getGeo(address, locatePoint) {
 
+//            toggles whether an address has shown a map to this point
+
             close_toggle++;
 
             var api = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+
+
+//            THIS KEY, PROVIDED BY NATHAN, HAS BEEN DISABLED
             var key = 'AIzaSyDP8RKElzU6BFGV205vH_0wkwWAtiGThus';
+
+//            creates url with google API and address (spaces removed with replace)
 
             var url = api + address.replace(/\s/g, '+')/* + '&key=' + key*/;
 
             $.get(url, function(data){
+
+//                   receives an object (DATA) from googlemaps and derives LAT/LNG
+
+
                 if(data.status && data.status === 'OK'){
                     locatePoint(data.results[0].geometry.location);
                 } else {
+//                    THIS OUGHT TO THROW AN ERROR OR SEND A DEFAULT LAT/LNG WITH DEFAULT LOCATION
                     locatePoint(data);
                 }
-                console.log(data);
             });
 
         };
 
+//          This function takes LAT/LNG coordinates and returns a map object from another googlemaps API call
+
         function locatePoint(data){
 
-            console.log('hello');
+//            Creates preferences that will be passed to google maps
+
             var latitude = data.lat;
             var longitude = data.lng;
             var myOptions = {
                 zoom: 17,
                 center: new google.maps.LatLng(latitude, longitude)
             };
-            console.log('help');
+
+//            calls for and renders map object from Googlemaps
+
             var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
 
         };
 
 
+//        Adds close button to rendered map if the button has not already been added
         if (close_toggle < 2) {
             $("#add").after("<button id='close_map'>CLOSE</button>");
         };
 
-
+//          REMOVES MAP AND BUTTON WITH A FADEOUT function (nice touch!)
         $("#close_map").click(function(){
             console.log($("#map-canvas"));
             $("#map-canvas").fadeOut();
@@ -206,23 +223,26 @@ $(function() {
             close_toggle = 0;
         });
 
-
-// LINK FOR ADDRESS LOOKUP THAT GIVES LAT&LONG
-// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
-
     };
+
+//  This function is called when add employee is clicked
 
 
     function add_employee(employee){
-
+//
+//        makes a POST call to the database to add new employee
         $.ajax({
             type: "POST",
             url: 'http://69.164.197.6/employees/',
             data: employee
         })
+//            WHEN SUCCESSFUL CALL IS MADE, PROMISES
+//            ARE INVOKED TO RERENDER TABLE
+
             .done(function( employees ) {
 
-                renewtable(employees)
+//                CALLS FUNCTION TO RERENDER DOM WITH COMPLETE TABLE
+                renew_table(employees)
 
                 });
 
@@ -232,7 +252,9 @@ $(function() {
 //
 
 
-        function renewtable(){
+        function renew_table(){
+//            AN AJAX CALL TO REPOPULATE EMPLOYEES OBJECT WITH WITH COMPLETE DATABASE
+//            A SUCCESSFUL CALL TRIGGERS A NEW RENDERING FUNCTION
             $.ajax({
                 url: 'http://69.164.197.6/employees/',
                 success: function(employees){
@@ -245,6 +267,7 @@ $(function() {
     }
 
 //    Render Table
+//    HTML FOR TABLE ROWS, INCLUDING BUTTONS AND ENABLING FUNCTIONS FOR ADDITIONAL FEATURES
 
     function render_employee_table(data) {
 
@@ -259,9 +282,10 @@ $(function() {
             html += "<td>" + e.name + "</td>";
             html += "<td>" + e.phone + "</td>";
             html += "<td>" + e.address + "</td>";
-            html += "<td><button class = 'delete btn btn-default btn-sm' index ='"+index+"'>Del</button></td>";
-            html += "<td><button class = 'edit btn btn-default btn-sm' index ='"+index+"'>Edit</button></td>";
-            html += "<td><button class = 'location btn btn-default btn-sm' index ='"+index+"'>Find Location</button></td>";
+            html += "<td>" + e.id + "</td>";
+            html += "<td><button class = 'delete btn btn-default btn-sm' index ='"+ index+"' index2 ='"+ e.id+"'>Del</button></td>";
+            html += "<td><button class = 'edit btn btn-default btn-sm' index ='"+index+"' index2 ='"+ e.id+"'>Edit</button></td>";
+            html += "<td><button class = 'location btn btn-default btn-sm' index ='"+ index+"' index2 ='"+ e.id+"'>Find Location</button></td>";
             html += "</tr>";
 
         })
@@ -270,25 +294,67 @@ $(function() {
 
         $('#employee_list').html(html);
 
+//        DELETE BUTTON FOR EACH INDEX
+
         $('.delete').click(function(){
-            console.log('delete clicked');
-            terminate_employee($(this).attr("index"));
+            console.log($(this).attr("index"), $(this).attr("index2"));
+//        PASSES INDEX VALUE TO DELETE FUNCTION
+            terminate_employee($(this).attr("index"), $(this).attr("index2"));
         });
+
+//        EDIT BUTTON FOR EACH INDEX
 
         $('.edit').click(function(){
             console.log('edit clicked');
 
             $('#edit_box').slideDown();
 
-            render_edit_box("edit", employees[$(this).attr("index")], $(this).attr("index"));
+//            PASSES EDIT INFO TO RENDER EDIT BOX WITH FLAG, EMPLOYEE OBJECT, AND INDEX
+
+            render_edit_box("edit", data[$(this).attr("index")], $(this).attr("index"), $(this).attr("index2"));
 
         });
+
+//           CALLS ON MAPPING FUNCTION WHILE PASSING EMPLOYEE OBJECT AND INDEX
 
         $('.location').click(function(){
+            console.log($(this).attr('index')+ "---" + data[$(this).attr('index')].id);
 
-            render_map(employees[$(this).attr("index")]);
+            render_map(data[$(this).attr("index")],$(this).attr("index"));
 
         });
+
+
+//  Function that removes data from table
+
+        function terminate_employee(index, id){
+
+//            defines endpoint that corresponds to employee data
+            goneURL = 'http://69.164.197.6/employees/'+id;
+
+//            MAKES AN AJAX CALL TO DELETE DATA FROM DATABASE AT ID ENDPOINT
+            $.ajax({
+                url: goneURL,
+                type: "DELETE"
+            })
+//            WHEN SUCCESSFUL CALL IS MADE, PROMISES
+//            ARE INVOKED TO RE-RENDER TABLE
+                .done(function( deleted_employee ) {
+                    console.log(deleted_employee);
+//                    MAKES AN AJAX REQUEST TO GET COMPLETE TABLE DATA FROM DATABASE
+                    $.ajax({
+                        url: 'http://69.164.197.6/employees/',
+                        success: function(employees){
+//                CALLS FUNCTION TO RE-RENDER DOM WITH COMPLETE TABLE
+                            render_employee_table(employees);
+                        }
+                    })
+                });
+
+
+            render_employee_table(data);
+
+        };
 
     }
 
